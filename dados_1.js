a9fb4e6a584a64fd9bb68844adf7d5ab03c81dfd
tlshudson <hudsonteles00@gmail.com>
2023-07-18 08:21:36 -0300

    4ª aula - Dados1

---
+++ dados_1.js
@@ -0,0 +1,14 @@
+var nome = "Caneta";
+var quantidade = 5;
+var imposto = 1.2
+var preco = 4.1
+var valorFinal = imposto + preco;
+
+console.log(nome);
+console.log(quantidade);
+console.log(imposto);
+console.log(preco);
+console.log(valorFinal);
+
+//aula 1 sobre estrutura de dados simples, armazenando dados literais(soltos) em variáveis,
+//dando valor/significado para cada dado.
