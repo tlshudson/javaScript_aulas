8c56457330bb25d2222f01d195e73d797b8cb585
tlshudson <hudsonteles00@gmail.com>
2023-07-20 08:34:41 -0300

    7ª aula desafioTroca

---
+++ desafio_troca.js
@@ -0,0 +1,12 @@
+let a = 7;
+let b = 94;
+
+//1° modo de trocar os valores das variáveis
+let temp = a;
+a = b;
+b = temp;
+
+//2° modo de trocar os valores das variáveis
+// [a, b] = [b, a]
+
+console.log(a, b);
