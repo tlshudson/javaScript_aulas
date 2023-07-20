c35047a9261e5eac5d3e9101c3299f509a8bf878
tlshudson <hudsonteles00@gmail.com>
2023-07-20 08:02:38 -0300

    5ªaula Dados3

---
+++ dados_3.js
@@ -0,0 +1,21 @@
+let idade = 31;
+console.log(typeof -31);
+console.log(typeof idade);
+/*mostrando alguns exemplos de tipos de dados,
+tanto armazenados em variáveis quanto dados soltos*/
+
+let salario = 12313.2332;
+console.log(typeof salario);
+/*dado do tipo number, porém chamado de ponto flutuante,
+pois as casas decimais podem se locomover tanto para a
+esquerda quanto para a direita*/
+
+let chuva = true;
+console.log(typeof chuva);
+/*Dados dos tipo booleano podem retornar dois valores:
+verdadeiro ou falso*/
+
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
+/*dados literais que são classificados do tipo string
+(caracteres)*/
