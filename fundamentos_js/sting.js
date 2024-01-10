a7736affe3baee0550b4d8fca7944c8b8bbaea4b
tlshudson <hudsonteles00@gmail.com>
2024-01-09 22:33:47 -0300

    10ª aula string.js

--- fundamentos_js/sting.js
+++ fundamentos_js/sting.js
@@ -3,3 +3,8 @@
 const escola = "Cod3r";
 
 console.log(escola);
+console.log(escola.charAt(4));
+console.log(escola.charCodeAt(3));
+console.log(escola.indexOf('3'));
+
+console.log(escola.substring(1));
