7edbbda85d3663417aa18a8a4d1a12c0bad2f8e6
tlshudson <hudsonteles00@gmail.com>
2024-01-18 23:25:45 -0300

    Aula sobre booleanos, parte I

---
+++ fundamentos_js/booleanos.js
@@ -0,0 +1,8 @@
+let isAtivo=  false;
+console.log(isAtivo);
+
+isAtivo = true;
+console.log(isAtivo); 
+
+//explicação do padrão do valor de variáveis que é sempre verdadeiro
+//utilização do ! para inverter o valor uma única vez, já o !! inverte o valor duas vezes, retornando ao seu valor original;
