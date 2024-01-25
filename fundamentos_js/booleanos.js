c08f88744d87f1201cd0ef55fafcf30a4ff5ad79
hudsontls <hudsontls@SUP-TI.lakeside.com>
2024-01-25 13:56:18 -0300

    Alterações e aula sobre booleanos

--- fundamentos_js/booleanos.js
+++ fundamentos_js/booleanos.js
@@ -5,4 +5,4 @@ isAtivo = true;
 console.log(isAtivo); 
 
 //explicação do padrão do valor de variáveis que é sempre verdadeiro
-//utilização do ! para inverter o valor uma única vez, já o !! inverte o valor duas vezes, retornando ao seu valor original;
+//utilização do ! para inverter o valor uma única vez, já o !! inverte o valor duas vezes, retornando ao seu valor original;n
