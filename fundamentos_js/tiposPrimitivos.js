baddaf88eb00c190afe56e506d9c6b19a1b6be78
tlshudson <hudsonteles00@gmail.com>
2024-05-22 23:39:13 -0300

    aula sobre tipos primitivos

---
+++ fundamentos_js/tiposPrimitivos.js
@@ -0,0 +1,19 @@
+//Um primitivo (valor primitivo, tipo de dados primitivo) é um dado que não é representado através de um Objeto e, por consequência, não possui métodos.
+
+//tipos primtivos: String, Number, Boolean, Null, Undefined, Symbol(Novo no EcmaScript)
+//todos os primitivos são imutáveis.
+
+var a = 15;
+console.log(typeof a);
+
+var b = undefined;
+console.log(typeof b);
+
+var c = false;
+console.log(typeof c);
+
+var d = "Bom dia";
+console.log(typeof d);
+
+var e = $;
+// console.log(typeof e);
