0ef9ad1bd7459c400512ff959f7a27ab6ae561a0
tlshudson <hudsonteles00@gmail.com>
2024-05-26 01:52:58 -0300

    aula nullUndefined concluída

--- fundamentos_js/null_Undefined.js
+++ fundamentos_js/null_Undefined.js
@@ -1,4 +1,12 @@
 let valor //não inicializada;
 
 console.log(valor);
-console.log(valor2);
+//por não estar inicializada, por padrão é definida como UNDEFINED
+
+// console.log(valor2);
+//este comando retorna erro pois a variável não é definida, retornando NOT DEFINED
+
+let valor3 = null; //variável nula, definida sem valor;
+
+let valorTest = "25";
+console.log(valorTest.toString());
