775c8b6b562527b1c1597f9a764f7448059c7e4f
tlshudson <hudsonteles00@gmail.com>
2024-05-26 02:13:11 -0300

    aula sobre função1 finalizada

---
+++ fundamentos_js/function1.js
@@ -0,0 +1,12 @@
+//função é utilizada em quase todas as linguagens, detém vários trechos/sentenças de código que podem ser chamados através do nome da função. Funciona como uma receita, recebe todos os dados e os trata de diversas maneiras de acordo com o escopo definido dentro da mesma.
+
+function imprimirSoma(a, b){
+    console.log(a + b);
+}
+
+//chamada de função + impressão do resultado
+imprimirSoma(1, 4);
+
+//caso de entrada de um só parâmetro(dado), dependendo do tipo do dado retorna algum valor como Undefined Not a Number (NaN);
+imprimirSoma(4);
+
