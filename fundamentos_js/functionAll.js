0826345f2e18aee2c6f4ca0e54465248d992250d
tlshudson <hudsonteles00@gmail.com>
2024-05-26 01:56:48 -0300

    aula sobre functionAll finalizada

---
+++ fundamentos_js/functionAll.js
@@ -0,0 +1,8 @@
+console.log(typeof Object);
+//Object é uma função 👀
+
+class Pessoa{}
+console.log(typeof Pessoa);
+//class é uma função também🤔
+
+//resumindo, toda classe em javascript é uma funcão, loucura!
