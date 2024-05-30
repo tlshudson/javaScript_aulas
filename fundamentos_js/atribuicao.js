7a3945e8f39d9c7a5d6f7e7d97a698e46821bf42
tlshudson <hudsonteles00@gmail.com>
2024-05-30 03:32:25 -0300

    em andamento aula atribuição =, vou dormir rs

---
+++ fundamentos_js/atribuicao.js
@@ -0,0 +1,14 @@
+//aula rápida sobre operadores de atribuição. O operador utilizado nesta aula foi o "=".
+//No exemplo abaixo inicilizarei 2 variáveis e irei atribuir 2 valores à elas.
+const a = 4;
+let b = 6;
+// console.log(b);
+
+//na linha 8 fiz a atribuição do valor da variável "b" + o valor da variável "a"
+b += a;
+console.log(b);
+
+//na linha abaixo farei a atribuição do valor da variável "b" - um valor aleatório, depois irei realizar a impressão desta atribuição
+b =- 3;
+console.log(b);
+
