24ecede0edd30e0e2fa1fb75cdcb289dcb47b374
tlshudson <hudsonteles00@gmail.com>
2024-07-31 01:05:29 -0300

    Aula sobre operadores ternarios finalizada

---
+++ fundamentos_js/ternario.js
@@ -0,0 +1,7 @@
+//exemplo de um operador ternário dentro de uma função de linha única
+             //relacional          //relacional
+const resultado = nota => nota >= 7 ? "Aprovado" : "Reprovado";
+//a junção de uma operadores relacional, e condicional, três partes;
+
+console.log(resultado(7.1));
+console.log(resultado(6.9));
