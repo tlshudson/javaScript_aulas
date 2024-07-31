e22f27a7312484fc18308082f4ae8af6a349a53d
tlshudson <hudsonteles00@gmail.com>
2024-07-31 00:30:05 -0300

    Aula sobre operadores aritméticos finalizada

---
+++ fundamentos_js/aritmeticos.js
@@ -0,0 +1,12 @@
+const [a, b, c, d] = [3, 5 ,4, 1];
+
+//operadores aritméticos sendo utilizados para atribuir valores em constantes:
+const soma = a + b + c + d;
+const subtracao = d - b;
+const multiplicacao = a * b;
+const divisao = d / a;
+
+//o operador modulo mostra o resto da divisão, sendo 1 retornando para valores impares e 2 para valores pares;
+const modulo = a % 2;
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo);
