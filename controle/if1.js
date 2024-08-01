9682fc8a0f45ff1d70abae2b181bc6b6457c1cd8
tlshudson <hudsonteles00@gmail.com>
2024-08-01 00:44:39 -0300

    Fim aula introdutória sobre IF

--- controle/if1.js
+++ controle/if1.js
@@ -2,8 +2,9 @@
 
 function boaNoticia(nota){
     if(nota >= 8){
-        console.log("Notícia boa" + nota);
+        console.log("Notícia boa: " + nota);
     }
 }
 
 boaNoticia(3);
+boaNoticia(8);
