eec6bfc0f586e31db9fd0119eb0ed8ce35dc1745
tlshudson <hudsonteles00@gmail.com>
2024-08-02 11:32:57 -0300

    Fim da aula sobre While

---
+++ controle/while.js
@@ -0,0 +1,17 @@
+//A declaração while cria um laço que executa uma rotina especifica enquanto a condição de teste for avaliada como verdadeira. A condição é avaliada antes da execução da rotina.
+
+//while (condição) {
+//    rotina
+// }
+
+//exemplo abaixo
+var n = 0;
+var x = 0;
+
+while (n < 3) {
+  n++;
+  x += n;
+}
+
+console.log(n);
+console.log(x);
