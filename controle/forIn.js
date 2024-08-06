93367ce53a34ec8c2d64fa5db7ce8dd7ef242747
tlshudson <hudsonteles00@gmail.com>
2024-08-06 14:44:18 -0300

    Fim da aula sobre ForIn

--- controle/forIn.js
+++ controle/forIn.js
@@ -4,5 +4,18 @@
 const notas = [2.2, 4.2, 5, 5.4, 6.3, 9.9]
 
 for(indice in notas){
-    console.log(indice)
+    console.log(`Notas ${indice}: ${notas[indice]}`);
 }
+//modelo de navegação usando for In
+
+const pessoa = {
+    nome: "Hudson",
+    idade: 54,
+    Patrimonio: 3203000,
+    filhos: 3
+}
+
+for(let atributos in pessoa){
+    console.log(pessoa[atributos])
+}
+//usando ForIn para percorrer um objeto, "maneira fácil e rápida de visualizar um ou mais objetos"
