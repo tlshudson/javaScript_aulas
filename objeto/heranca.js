c18d27dc8887560c5ac014c17ddb68f5bade2f75
tlshudson <hudsonteles00@gmail.com>
2025-06-11 14:45:45 -0300

    aula de herança

---
+++ objeto/heranca.js
@@ -0,0 +1,26 @@
+function Person(first, last, age, gender, interests) {
+    this.name = {
+        first, last
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+}
+
+function Teacher(first, last, age, gender, interests, subject) {
+    //herdar propriedades do construtor Person
+    Person.call(this, first, last, age, gender, interests);
+
+    //propriedade especifica de Teacher
+    this.subject = subject;
+};
+//herdar metodos prototype do Person
+Teacher.prototype = Object.create(Person.prototype);
+
+//corrigir o construtor
+Teacher.prototype.constructor = Teacher;
+
+Teacher.prototype.greeting = function () {
+    console.log("Hello, my name is: " + this.name.first + " and I teach" + this.subject + ".");
+    
+}
