8abdc0df20dbb73f2a33e37af9b67e7942b5a064
tlshudson <hudsonteles00@gmail.com>
2026-03-31 17:46:43 -0300

    aula de objeto global

---
+++ node/globalCliente.js
@@ -0,0 +1,5 @@
+require('./global');
+
+console.log(MinhaApp.saudacao());
+console.log(MinhaApp.nome);
+//estratégia de definição de objetos globais sendo declarados em outro arquivo e invocados aqui através do require
