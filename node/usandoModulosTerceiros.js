ba5f67b890efea65e175444e10fa7b7f0deaf277
tlshudson <hudsonteles00@gmail.com>
2026-03-31 00:43:23 -0300

    aula modulos terceiros

---
+++ node/usandoModulosTerceiros.js
@@ -0,0 +1,4 @@
+const _ = require('lodash');
+setInterval(() => console.log(_.random(1, 1000)), 2000)
+
+//utilização do lodash através de uma instalação no projeto local e a instalação do nodemon no global do PC
